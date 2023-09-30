867175a7e76232d71f89767c5b9f2e33a35ec2fa
Victoria Sass <vsass@uw.edu>
2023-09-30 09:49:18 -0700

    updating date scheduler script to be a function

--- Lectures/due_dates_schedule.R
+++ Lectures/due_dates_schedule.R
@@ -1,5 +1,5 @@
 
-# Creating base schedule table for CSSS 508
+# Creating base schedule table function for CSSS 508
 
 library(gt)
 library(tidyverse)
@@ -10,25 +10,42 @@ schedule <- tibble(Homework = c(1:9),
                    Due_Date_AB = c("12 October", "19 October", "26 October", "2 November", "9 November", "16 November", "23 November", "30 November", "7 December"), 
                    Peer_Due_Date_AB = c("17 October", "24 October", "31 October", "7 November", "14 November", "21 November", "28 November", "5 November", "12 December"))
 
-base_schedule <- schedule |> 
-  gt() |> 
-  # Group sections AA and AB together
-  tab_spanner(label = md("**Section AA**"),
-              columns = vars(Due_Date_AA, Peer_Due_Date_AA)) |> 
-  tab_spanner(label = md("**Section AB**"),
-              columns = vars(Due_Date_AB, Peer_Due_Date_AB), 
-              id = "AB") |> 
-  cols_align(align = "center") |> 
-  cols_label(Homework = "#",
-             Due_Date_AA = "Homework Due",
-             Peer_Due_Date_AA = "Peer Review Due",
-             Due_Date_AB = "Homework Due",
-             Peer_Due_Date_AB = "Peer Review Due") |> 
-  # aligning table on page, picking font, increasing overall table size
-  tab_options(table.align = "center", table.font.names = c("Quattrocento", "Raleway"), 
-              table.font.size = pct(100), table.border.top.style = "hidden", table.border.bottom.style = "hidden") |>
-  # Adding color to column names
-  tab_style(style = cell_fill(color = "#c7cdac"),
-            locations = cells_column_labels(columns = c(Due_Date_AA, Due_Date_AB))) |> 
-  tab_style(style = cell_fill(color = "#99a486"),
-            locations = cells_column_labels(columns = c(Peer_Due_Date_AA, Peer_Due_Date_AB)))
+make_due_date_table <- function(week){
+  due_dates_table <- schedule |> 
+    gt() |> 
+    # Group sections AA and AB together
+    tab_spanner(label = md("**Section AA**"),
+                columns = vars(Due_Date_AA, Peer_Due_Date_AA)) |> 
+    tab_spanner(label = md("**Section AB**"),
+                columns = vars(Due_Date_AB, Peer_Due_Date_AB), 
+                id = "AB") |> 
+    cols_align(align = "center") |> 
+    cols_label(Homework = "#",
+               Due_Date_AA = "Homework Due",
+               Peer_Due_Date_AA = "Peer Review Due",
+               Due_Date_AB = "Homework Due",
+               Peer_Due_Date_AB = "Peer Review Due") |> 
+    # aligning table on page, picking font, increasing overall table size
+    tab_options(table.align = "center", table.font.names = c("Quattrocento", "Raleway"), 
+                table.font.size = pct(100), table.border.top.style = "hidden", table.border.bottom.style = "hidden") |>
+    # Adding color to column names and due date row
+    tab_style(style = cell_fill(color = "#c7cdac"),
+              locations = list(cells_column_labels(columns = c(Due_Date_AA, Due_Date_AB)), 
+                               cells_body(columns = c(Due_Date_AA, Due_Date_AB), 
+                                          rows = Homework == week))) |> 
+    tab_style(style = cell_fill(color = "#99a486"),
+              locations = list(cells_column_labels(columns = c(Peer_Due_Date_AA, Peer_Due_Date_AB)), 
+                               cells_body(columns = c(Peer_Due_Date_AA, Peer_Due_Date_AB), 
+                                          rows = Homework == week))) |> 
+    # adding borders around due dates
+    tab_style(style = cell_borders(sides = c("bottom", "top"),  weight = px(3), color = "#1e4655"),
+              locations = cells_body(rows = Homework == week)) |> 
+    tab_style(style = cell_borders(sides = "left",  weight = px(3), color = "#1e4655"), 
+              locations = cells_body(rows = Homework == week, 
+                                     columns = Homework)) |> 
+    tab_style(style = cell_borders(sides = "right",  weight = px(3), color = "#1e4655"), 
+              locations = cells_body(rows = Homework == week, 
+                                     columns = Peer_Due_Date_AB))
+  
+  return(due_dates_table)
+}
