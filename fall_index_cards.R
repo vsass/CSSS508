3807426eca2dedb2694bd2f4d772ce42a225ff7c
Victoria Sass <vsass@uw.edu>
2023-10-09 00:20:49 -0700

    Updating lecture 2

--- fall_index_cards.R
+++ fall_index_cards.R
@@ -13,3 +13,13 @@ fall_2023 <- tribble(
   "e", "he, him", "MSW/MPH", 3, "Last year was my first time learning R", "Excited", "Lifelong fluency in every human language", 
   
 )
+
+::: {.callout-note icon=false}
+## <span style="color:blue">{{< fa hand-finger-up >}}</span> Facet scales options
+By default, facets will share the same axis ranges (i.e. "fixed") which is helpful for comparison across groups. However, sometimes you may want to visualize the relationship within each facet better. You can set the `scales` argument to "free", "free_y" or "free_x" to allow scales to vary by both `x` and `y` axes, or just one. 
+:::
+  
+  
+ggplot(data = gapminder) + 
+  geom_line(aes(x = year , y = gdpPercap, color = continent, group = country)) + 
+  facet_grid(cols = vars(continent))
