2dc7e6f630867b00d28949fc12a248d135a3eb73
Victoria Sass <vsass@uw.edu>
2025-09-19 12:05:20 -0700

    Fall 2025 Initial Update

--- Homework/due_dates_schedule.R
+++ Homework/due_dates_schedule.R
@@ -5,10 +5,8 @@ library(gt)
 library(tidyverse)
 
 schedule_fall <- tibble(Homework = c(1:9), 
-                             Due_Date_AA = c("10 October", "17 October", "24 October", "31 October", "7 November", "14 November", "21 November", "28 November", "5 December"), 
-                             Peer_Due_Date_AA = c("15 October", "22 October", "29 October", "5 November", "12 November", "19 November", "26 November", "3 November", "10 December"), 
-                             Due_Date_AB = c("12 October", "19 October", "26 October", "2 November", "9 November", "16 November", "23 November", "30 November", "7 December"), 
-                             Peer_Due_Date_AB = c("17 October", "24 October", "31 October", "7 November", "14 November", "21 November", "28 November", "5 November", "12 December"))
+                             Due_Date = c("7 October", "14 October", "21 October", "28 October", "11 November", "18 November", "25 November", "2 December", "9 December"), 
+                             Peer_Due_Date = c("12 October", "19 October", "26 October", "2 November", "16 November", "21 November", "30 November", "7 December", "14 December"))
 
 schedule_spring <- tibble(Homework = c(1:9), 
                                Due_Date = c("2 April", "9 April", "16 April", "23 April", "30 April", "7 May", "14 May", "21 May", "28 May"), 
@@ -18,29 +16,24 @@ schedule_spring <- tibble(Homework = c(1:9),
 make_due_date_table_fall <- function(week){
   due_dates_table <- schedule_fall |> 
     gt() |> 
-    # Group sections AA and AB together
-    tab_spanner(label = md("**Section AA**"),
-                columns = c(Due_Date_AA, Peer_Due_Date_AA)) |> 
-    tab_spanner(label = md("**Section AB**"),
-                columns = c(Due_Date_AB, Peer_Due_Date_AB), 
-                id = "AB") |> 
     cols_align(align = "center") |> 
     cols_label(Homework = "#",
-               Due_Date_AA = "Homework Due",
-               Peer_Due_Date_AA = "Peer Review Due",
-               Due_Date_AB = "Homework Due",
-               Peer_Due_Date_AB = "Peer Review Due") |> 
+               Due_Date = "Homework Due",
+               Peer_Due_Date = "Peer Review Due") |> 
     # aligning table on page, picking font, increasing overall table size
     tab_options(table.align = "center", table.font.names = c("Quattrocento", "Raleway"), 
                 table.font.size = pct(100), table.border.top.style = "hidden") |>
+    # make column labels bold
+    tab_style(style = cell_text(weight = "bolder", size = "large"), 
+              locations = cells_column_labels(columns = everything())) |>
     # Adding color to column names and due date row
     tab_style(style = cell_fill(color = "#c7cdac"),
-              locations = list(cells_column_labels(columns = c(Due_Date_AA, Due_Date_AB)), 
-                               cells_body(columns = c(Due_Date_AA, Due_Date_AB), 
+              locations = list(cells_column_labels(columns = c(Due_Date)), 
+                               cells_body(columns = c(Due_Date), 
                                           rows = Homework == week))) |> 
-    tab_style(style = cell_fill(color = "#99a486"),
-              locations = list(cells_column_labels(columns = c(Peer_Due_Date_AA, Peer_Due_Date_AB)), 
-                               cells_body(columns = c(Peer_Due_Date_AA, Peer_Due_Date_AB), 
+    tab_style(style = list(cell_fill(color = "#99a486"), cell_text(color = "white")),
+              locations = list(cells_column_labels(columns = c(Peer_Due_Date)), 
+                               cells_body(columns = c(Peer_Due_Date), 
                                           rows = Homework == week))) |> 
     # adding borders around due dates
     tab_style(style = cell_borders(sides = c("bottom", "top"),  weight = px(3), color = "#1e4655"),
@@ -50,10 +43,12 @@ make_due_date_table_fall <- function(week){
                                      columns = Homework)) |> 
     tab_style(style = cell_borders(sides = "right",  weight = px(3), color = "#1e4655"), 
               locations = cells_body(rows = Homework == week, 
-                                     columns = Peer_Due_Date_AB))
-  
+                                     columns = Peer_Due_Date)) |> 
+    cols_width(Homework ~ pct(10), 
+               contains("Date") ~ px(45)) |> 
+    tab_options(table.width = pct(65), table.font.size = pct(105)) 
   return(due_dates_table)
-}
+}  
 
 make_due_date_table_spring <- function(week){
   due_dates_table <- schedule_spring |> 
@@ -62,6 +57,9 @@ make_due_date_table_spring <- function(week){
     cols_label(Homework = "#",
                Due_Date = "Homework Due",
                Peer_Due_Date = "Peer Review Due") |> 
+    # make column labels bold
+    tab_style(style = cell_text(weight = "bolder", size = "large"), 
+              locations = cells_column_labels(columns = everything())) |>
     # aligning table on page, picking font, increasing overall table size
     tab_options(table.align = "center", table.font.names = c("Quattrocento", "Raleway"), 
                 table.font.size = pct(100), table.border.top.style = "hidden") |>
--- Homework/rubric.R
+++ Homework/rubric.R
@@ -6,9 +6,9 @@ library(gt)
 ## homework
 
 rubric_table <- tibble(Evaluation = c("Didn't turn anything in.", 
-                                "Turned in but low effort, ignoring many directions.", 
-                                "Decent effort, followed directions with some minor issues.", 
-                                "Nailed it!"), 
+                                "Turned in but low effort, <br> ignoring many directions.", 
+                                "Decent effort, followed directions <br> with some minor issues.", 
+                                "Nailed it!") |> str_wrap(width = 50), 
                  Points = 0:3)
 
 rubric <- rubric_table |>  
@@ -16,17 +16,21 @@ rubric <- rubric_table |>
   tab_options(table.font.names = c("Quattrocento", "Raleway")) |>
   cols_align(align = "center") |> 
   # Color each evaluation
-  tab_style(style = cell_fill(color = "#e15759"),
+  tab_style(style = list(cell_fill(color = "#f15a30"), 
+                         cell_text(color = "white", size = "large")),
             locations = cells_body(rows = Points == 0)) |> 
-  tab_style(style = cell_fill(color = "#f28e2c"),
+  tab_style(style = list(cell_fill(color = "#8b835b"), 
+                         cell_text(color = "white", size = "large")),
             locations = cells_body(rows = Points == 1)) |> 
-  tab_style(style = cell_fill(color = "#76b7b2"),
+  tab_style(style = list(cell_fill(color = "#5a82b3"), 
+                         cell_text(color = "white", size = "large")),
             locations = cells_body(rows = Points == 2)) |> 
-  tab_style(style = cell_fill(color = "#4e79a7"),
+  tab_style(style = list(cell_fill(color = "#9a72aa"), 
+                         cell_text(color = "white", size = "large")),
             locations = cells_body(rows = Points == 3)) |> 
-  # Change label font style
-  cols_label(Evaluation = md("**Evaluation**"), 
-             Points = md("**Points**")) |> 
+  # make column labels bold
+  tab_style(style = cell_text(weight = "bolder", size = "x-large", stretch = "expanded"), 
+            locations = cells_column_labels(columns = everything())) |>
   # Adding border around label row
   tab_style(style = cell_borders(sides = "top",  weight = px(2)),
             locations = cells_column_labels()) |> 
@@ -37,14 +41,18 @@ rubric <- rubric_table |>
             locations = list(cells_column_labels(columns = "Points"), 
                              cells_body(columns = "Points"))) |> 
   tab_style(style = cell_borders(sides = "bottom",  weight = px(2)),
-            locations = cells_body(rows = Points == 3))
+            locations = cells_body(rows = Points == 3)) |> 
+  fmt_markdown(columns = everything()) |> 
+  cols_width(Points ~ pct(10), 
+             Evaluation ~ pct(25)) |> 
+  opt_vertical_padding(scale = 1.15)
 
 ## peer review
 
 peer_review_table <- tibble(Evaluation = c("Didn't follow all peer-review instructions.", 
-                                           "Peer review is at least several sentences long, <br> 
-                                           mentions any and all key issues from the assignment, <br> 
-                                           and points out at least one positive thing in your <br> 
+                                           "Peer review is at least several sentences long, 
+                                           mentions any and all key issues from the assignment, 
+                                           and points out at least one positive thing in your 
                                            peer’s work (and hopefully more!)."),
                             Points = 0:1)
 
@@ -52,14 +60,15 @@ peer_review_rubric <- peer_review_table |>
   gt() |> 
   tab_options(table.font.names = c("Quattrocento", "Raleway")) |>
   cols_align(align = "center") |> 
+  # Specify column widths
   # Color each evaluation
-  tab_style(style = cell_fill(color = "#59a14f"),
+  tab_style(style = list(cell_fill(color = "#4e1d4c"), cell_text(color = "white")),
             locations = cells_body(rows = Points == 1)) |> 
-  tab_style(style = cell_fill(color = "#edc949"),
+  tab_style(style = list(cell_fill(color = "#f99d1b"), cell_text(color = "black")),
             locations = cells_body(rows = Points == 0)) |> 
-  # Change label font style
-  cols_label(Evaluation = md("**Evaluation**"), 
-             Points = md("**Points**")) |> 
+  # make column labels bold
+  tab_style(style = cell_text(weight = "bolder", size = "x-large", stretch = "expanded"), 
+            locations = cells_column_labels(columns = everything())) |>
   # Adding border around label row
   tab_style(style = cell_borders(sides = "top",  weight = px(2)),
             locations = cells_column_labels()) |> 
@@ -71,4 +80,8 @@ peer_review_rubric <- peer_review_table |>
                              cells_body(columns = "Points"))) |> 
   tab_style(style = cell_borders(sides = "bottom",  weight = px(2)),
             locations = cells_body(rows = Points == 1)) |> 
-  fmt_markdown(columns = TRUE)
+  fmt_markdown(columns = everything()) |> 
+  cols_width(Points ~ pct(10), 
+             Evaluation ~ pct(25)) |> 
+  opt_vertical_padding(scale = 1.25)
+
